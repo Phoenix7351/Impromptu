29796bee3aae8712c1c383abbecb730b61563778
Phoenix7351 <Phoenix@Justins-Mac-mini.local>
2015-01-12 20:29:40 -0600

    Starter project

---
+++ Impromptu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Impromptu
+//
+//  Created by Justin Tiffner on 1/12/15.
+//  Copyright (c) 2015 Justin Tiffner. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
