90d4b99c6da3668cebdc77b4f4f092c853b1fde1
Phoenix7351 <Phoenix@Justins-Mac-mini.local>
2015-01-14 21:27:32 -0600

    Messages are written to the server via the submit button and read from the server to the table view.

--- Impromptu/ViewController.m
+++ Impromptu/ViewController.m
@@ -9,52 +9,108 @@
 #import "ViewController.h"
 #import <CloudKit/CloudKit.h>
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (strong, nonatomic) CKDatabase *ckDatabase;
 
 @property (weak, nonatomic) IBOutlet UITextField *userMessage;
 @property (weak, nonatomic) IBOutlet UITableView *messageList;
-@property (weak, nonatomic) NSMutableArray *messages;
+@property (strong, nonatomic) NSMutableArray *messages;
 - (IBAction)textEntered:(id)sender;
 
 @end
 
 @implementation ViewController
 
+NSString * const kMessageRecordType = @"Message";
+NSString * const kMessageRecordTextBodyAttribute = @"TextBody";
+NSString * const kMessageRecordLocationAttribute = @"Location";
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    CKRecord *message = [[CKRecord alloc] initWithRecordType:@"Message"];
-    
-    CKContainer *container = [CKContainer defaultContainer];
-    
-    CKDatabase *database = [container publicCloudDatabase];
+//    CKRecord *message = [[CKRecord alloc] initWithRecordType:@"Message"];
+//    
+//    CKContainer *container = [CKContainer defaultContainer];
+//    
+//    CKDatabase *database = [container publicCloudDatabase];
+//    
+//    CKQuery *query = [[CKQuery alloc] initWithRecordType:@"Message" predicate:[NSPredicate predicateWithFormat:@"TRUEPREDICATE"]];
+//    
+//    CKQueryOperation *operation = [[CKQueryOperation alloc] initWithQuery:query];
+//    operation.recordFetchedBlock = ^(CKRecord *record) {
+//        NSLog(@"s word");
+//    };
+//    
+//    [database addOperation:operation];
+//    
+//    CKRecordZone *rz = [CKRecordZone defaultRecordZone];
+//    [database performQuery:query inZoneWithID:rz.zoneID completionHandler:^(NSArray *results, NSError *error) {
+//        NSLog(@"Take a look at who?");
+//    }];
+//    
+//    NSLog(@"Pen is");
     
     CKQuery *query = [[CKQuery alloc] initWithRecordType:@"Message" predicate:[NSPredicate predicateWithFormat:@"TRUEPREDICATE"]];
-    
-    CKQueryOperation *operation = [[CKQueryOperation alloc] initWithQuery:query];
-    operation.recordFetchedBlock = ^(CKRecord *record) {
-        NSLog(@"s word");
-    };
-    
-    [database addOperation:operation];
-    
     CKRecordZone *rz = [CKRecordZone defaultRecordZone];
-    [database performQuery:query inZoneWithID:rz.zoneID completionHandler:^(NSArray *results, NSError *error) {
-        NSLog(@"Take a look at who?");
+    [self.ckDatabase performQuery:query inZoneWithID:rz.zoneID completionHandler:^(NSArray *results, NSError *error) {
+        self.messages = [NSMutableArray arrayWithArray:results];
+        dispatch_async(dispatch_get_main_queue(), ^{
+           [self.messageList reloadData];
+        });
     }];
     
-    NSLog(@"Pen is");
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (IBAction)textEntered:(id)sender {
     //messages
+    NSString *messageString = self.userMessage.text;
+    
+    CKRecord *newMessageRecord = [[CKRecord alloc] initWithRecordType:kMessageRecordType];
+    
+    newMessageRecord[kMessageRecordTextBodyAttribute] = messageString;
+    
+    //Use real location!
+    CLLocation *location = [[CLLocation alloc] initWithLatitude:32.00 longitude:-132.456];
+    
+    newMessageRecord[kMessageRecordLocationAttribute] = location;
+    
+    [self.ckDatabase saveRecord:newMessageRecord completionHandler:^(CKRecord *record, NSError *error) {
+        if (error == nil) {
+            NSLog(@"Message Saved to cloud");
+        }
+    }];
+    
+    self.userMessage.text = @"";
+    
     [_userMessage resignFirstResponder];
     
 }
+
+#pragma mark Table Source Delegate
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.messages.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"messageCell"];
+    
+    CKRecord *message = [self.messages objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = message[kMessageRecordTextBodyAttribute];
+    
+    return cell;
+}
+
+#pragma mark Lazy loaders
+
+- (CKDatabase *)ckDatabase {
+    if (_ckDatabase == nil) {
+        _ckDatabase = [[CKContainer defaultContainer] publicCloudDatabase];
+    }
+    return _ckDatabase;
+}
+
 @end
